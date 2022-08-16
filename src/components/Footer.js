ebfd81c8b1ab54ffbff2ad858ccab0bf0bc852d0
HyungJoon <itineris24@naver.com>
2022-08-16 03:02:00 -0700

    Home: background and card props created

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import {Container, Row, Col } from "react-bootstrap";
+
+const styleFooter = {
+  backgroundColor: "#F8F8F8",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%",
+};
+
 
 export default function Footer() {
-  return (
-    <Container>
+  return (  
+    <Container fluid style={styleFooter}>
       <Row>
-        <Col>
-          <p>Designed and developed by </p>
+        <Col md={9} style={{display:'flex', justifyContent:'right'}}>  
+          <p>Designed and developed by SMJHSNHJL</p>
         </Col>
-        <Col>
+        <Col md={2} style={{display:'flex', justifyContent:'right'}}>
           <p>Copyright © 2022</p>
         </Col>
-        <Col>
+        <Col md={1} style={{display:'flex', justifyContent:'left'}}>
           <p>Social icons</p>
         </Col>
       </Row>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,46 @@
 import React from "react";
+import CardItem from './CardItem';
+import "./Home.css";
+import sampleImage from '../../img/sample.png'
+
+ 
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="intro">We are Devs in Vancouver</div>
+      
+      <div className='cards'>
+        <div className="cards__container">
+          <div className="cards__wrapper">  
+            <ul className='cards__items'>
+              <CardItem
+                src={sampleImage}
+                text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                label='Somi JEON'
+                path='/resume/somijeon'
+              />
+              <CardItem
+                src={sampleImage}
+                text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                label='HyonSok NA'
+                path='/resume/hyonsokna'
+              />
+              <CardItem
+                src={sampleImage}
+                text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                label='HyungJoon LEE'
+                path='/resume/hyungjoonlee'
+              />   
+            </ul>    
+          </div>
+        </div>            
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
+     </div>
+  );
 }
