1f00a0d146da4d78e297a047ec31828d785c51ea
Hyonsok <46020558+Hyonsok@users.noreply.github.com>
2022-08-15 20:15:40 -0700

    initial projects page

---
+++ src/pages/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import './Projects.css';
+
+
+function Projects() {
+  return (
+    <div className='hero-container'>
+      <form>
+        </form>
+    </div>
+  );
+}
+
+export default Projects;
