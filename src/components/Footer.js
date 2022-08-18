de29ef47cfe3f613b2db6fe539fe225524886de7
TripToCodes <iam.somj@gmail.com>
2022-08-18 00:37:15 -0700

    [Feat] add social icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,46 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { BsFileEarmarkPpt } from "react-icons/bs";
 
 export default function Footer() {
   return (
     <Container>
       <Row>
-        <Col>
+        <Col className="flex justify-center">
           <p>Designed and developed by </p>
         </Col>
-        <Col>
+        <Col className="flex justify-center">
           <p>Copyright © 2022</p>
         </Col>
-        <Col>
-          <p>Social icons</p>
+        <Col className="flex justify-center">
+          <Row className="inline-block">
+            <a
+              href="https://github.com/TripToCodes"
+              target="_blank"
+              rel="noreferrer"
+              className="w-auto"
+            >
+              <AiFillGithub className="text-black" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/somi-jeon/"
+              target="_blank"
+              rel="noreferrer"
+              className="w-auto"
+            >
+              <FaLinkedinIn className="text-black" />
+            </a>
+            <a
+              href="https://github.com/codestates/HappyDeving/blob/dev3/README_en.md"
+              target="_blank"
+              rel="noreferrer"
+              className="w-auto"
+            >
+              <BsFileEarmarkPpt className="text-black" />
+            </a>
+          </Row>
         </Col>
       </Row>
     </Container>
