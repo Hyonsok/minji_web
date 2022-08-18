a6044be47fdff922ac01a40c10263f83b2b536cc
Hyonsok <46020558+Hyonsok@users.noreply.github.com>
2022-08-18 01:06:26 -0700

    created a github activity

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,9 +4,32 @@ import { DiReact } from "react-icons/di"
 import { DiNodejs } from "react-icons/di"
 import { DiJavascript1 } from "react-icons/di"
 import { DiJava } from "react-icons/di"
-
+import Calendar from 'react-github-contribution-calendar';
 import './About.css'
+
+<link rel="stylesheet" href="node_modules/react-github-contribution-calendar/default.css" type="text/css" />
 function Projects() {
+    var values = {
+        '2022-06-23': 1,
+        '2022-06-26': 2,
+        '2022-06-27': 3,
+        '2022-06-28': 4,
+        '2022-06-29': 4
+      }
+      var until = '2022-06-30';
+      var panelColors = [
+        '#EEEEEE',
+        '#F78A23',
+        '#F87D09',
+        '#AC5808',
+        '#7B3F06'
+      ]
+      var weekNames = ['', 'Mon', '', 'Wed', '', 'Fri', ''];
+    var monthNames = [
+        'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan',
+        'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'
+    ];
+    
   return (
     <div className='about-container'>
         <h3>About page</h3>
@@ -23,6 +46,11 @@ function Projects() {
             <div className='tech-icons col-md-2 col-4'>
                 <DiJava/>
             </div>
+        </Row>
+        <Row>
+        <Calendar
+        values={values} until={until} panelColors={panelColors}
+        weekNames={weekNames} monthNames={monthNames}/> 
         </Row>
     </div>
   );
