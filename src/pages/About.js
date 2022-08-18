1a61cd57bbb3dae89be8c49169d1173608765151
Hyonsok <46020558+Hyonsok@users.noreply.github.com>
2022-08-18 00:45:39 -0700

    added dev icons

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row'
+import { DiReact } from "react-icons/di"
+import { DiNodejs } from "react-icons/di"
+import { DiJavascript1 } from "react-icons/di"
+import { DiJava } from "react-icons/di"
 
-
+import './About.css'
 function Projects() {
   return (
-    <div className='hero-container'>
+    <div className='about-container'>
         <h3>About page</h3>
+        <Row>
+            <div className='tech-icons col-md-2 col-4'>
+                <DiReact/>
+            </div>
+            <div className='tech-icons col-md-2 col-4'>
+                <DiNodejs/>
+            </div>
+            <div className='tech-icons col-md-2 col-4'>
+                <DiJavascript1/>
+            </div>
+            <div className='tech-icons col-md-2 col-4'>
+                <DiJava/>
+            </div>
+        </Row>
     </div>
   );
 }
