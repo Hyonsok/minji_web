3e0de1257e79102c7538e8e3b477961568ec9fa3
Hyonsok <46020558+Hyonsok@users.noreply.github.com>
2022-08-18 00:13:11 -0700

    created an about page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Projects from './pages/Projects'
+import About from './pages/About'
 
 function App() {
   return (
     <>
     <Router>
       <Routes>
-        <Route exact path='/' element={<Projects/>}/>
+        <Route exact path='/about' element={<About/>}/>
+        <Route exact path='/projects' element={<Projects/>}/>
       </Routes>
     </Router>
     </>
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Projects() {
+  return (
+    <div className='hero-container'>
+        <h3>About page</h3>
+    </div>
+  );
+}
+
+export default Projects;
