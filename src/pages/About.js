5d2df061ed8a6cb62c67981dbdd37823bf3493bb
Hyonsok <46020558+Hyonsok@users.noreply.github.com>
2022-08-19 14:42:14 -0700

    solved About page merging conflict

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,44 +1,16 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import Row from 'react-bootstrap/Row'
-import { DiReact } from "react-icons/di"
-import { DiNodejs } from "react-icons/di"
-import { DiJavascript1 } from "react-icons/di"
-import { DiJava } from "react-icons/di"
+import Skillset from '../components/Skillset';
+import Tool from '../components/Tool';
+import GithubActivity from '../components/GithubActivity';
 
 import './About.css'
 function Projects() {
-  return (
-    <div className='about-container'>
-        <h3>About page</h3>
-        <Row>
-            <div className='tech-icons col-md-2 col-4'>
-                <DiReact/>
-            </div>
-            <div className='tech-icons col-md-2 col-4'>
-                <DiNodejs/>
-            </div>
-            <div className='tech-icons col-md-2 col-4'>
-                <DiJavascript1/>
-            </div>
-            <div className='tech-icons col-md-2 col-4'>
-                <DiJava/>
-            </div>
-        </Row>
-=======
-import Skillset  from '../components/Skillset';
-import Tool from '../components/Tool'
-import GithubActivity from '../components/GithubActivity';
-function Projects() {
-    
-    
   return (
     <div className='about-container'>
         <Skillset/>
         <br/>
         <Tool />
         <GithubActivity/>
->>>>>>> Stashed changes
     </div>
   );
 }
